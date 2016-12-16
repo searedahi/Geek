5cb14554f1309e3dba2403e8a2cf7a2dd7bc4d40
seare <seare@GEEKYLAPTOP>
2016-12-15 19:00:22 -0800

    now the big stuff

---
+++ GeekyInc.Web/Models/GeekyUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace GeekyInc.Web.Models
+{
+    // Add profile data for application users by adding properties to the GeekyUser class
+    public class GeekyUser : IdentityUser
+    {
+    }
+}
---
+++ GeekyInc.Web/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekyInc.Web.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
